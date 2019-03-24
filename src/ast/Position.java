5aafd01100e1ba098b1906718adcf5912133a735
FlypigQian <qiansucheng2018@outlook.com>
2019-03-24 11:35:16 +0800

    remove outdated code completely

--- src/ast/Position.java
+++
@@ -1,6 +0,0 @@
-package ast;
-
-public class Position {
-  public int rowNo;
-  public int colNo;
-}
--- src/ast/node/exp/literal/LiteralExp.java
+++
@@ -1,7 +0,0 @@
-package ast.node.exp.literal;
-
-import ast.node.exp.Exp;
-
-abstract public class LiteralExp extends Exp {
-
-}
