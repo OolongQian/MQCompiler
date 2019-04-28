7aaacdb17a10b47de2ea924bc896b47705bf103f
FlypigQian <qiansucheng2018@outlook.com>
2019-04-28 14:13:39 +0800

    fix antlr grammar error

--- src/antlr_tools/MgLexer.java
+++ src/antlr_tools/MgLexer.java
@@ -1,4 +1,4 @@
-package antlr_tools;// Generated from Mg.g4 by ANTLR 4.7.1
+package antlr_tools;// Generated from Mg.g4 by ANTLR 4.7.2
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
@@ -10,7 +10,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class MgLexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.7.1", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.7.2", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
@@ -33,36 +33,45 @@ public class MgLexer extends Lexer {
 		"DEFAULT_MODE"
 	};
 
-	public static final String[] ruleNames = {
-		"T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "T__6", "T__7", "T__8", 
-		"T__9", "T__10", "T__11", "T__12", "T__13", "T__14", "T__15", "T__16", 
-		"T__17", "T__18", "T__19", "T__20", "T__21", "T__22", "T__23", "T__24", 
-		"T__25", "T__26", "T__27", "T__28", "T__29", "T__30", "T__31", "LogicConstant", 
-		"PosIntegerConstant", "Letter", "Underscore", "DigitNoZero", "Digit", 
-		"StringConstant", "StringCharacters", "StringCharacter", "EscapeSequence", 
-		"LineComment", "BlockComment", "WhiteSpace", "NewLine", "Bool", "Int", 
-		"String", "Void", "Null", "True", "False", "If", "Else", "For", "While", 
-		"Break", "Continue", "Return", "New", "Class", "This", "Identifier"
-	};
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "T__6", "T__7", "T__8", 
+			"T__9", "T__10", "T__11", "T__12", "T__13", "T__14", "T__15", "T__16", 
+			"T__17", "T__18", "T__19", "T__20", "T__21", "T__22", "T__23", "T__24", 
+			"T__25", "T__26", "T__27", "T__28", "T__29", "T__30", "T__31", "LogicConstant", 
+			"PosIntegerConstant", "Letter", "Underscore", "DigitNoZero", "Digit", 
+			"StringConstant", "StringCharacters", "StringCharacter", "EscapeSequence", 
+			"LineComment", "BlockComment", "WhiteSpace", "NewLine", "Bool", "Int", 
+			"String", "Void", "Null", "True", "False", "If", "Else", "For", "While", 
+			"Break", "Continue", "Return", "New", "Class", "This", "Identifier"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
 
-	private static final String[] _LITERAL_NAMES = {
-		null, "';'", "'('", "')'", "','", "'='", "'{'", "'}'", "'++'", "'--'", 
-		"'+'", "'-'", "'!'", "'~'", "'*'", "'/'", "'%'", "'<<'", "'>>'", "'>'", 
-		"'>='", "'<'", "'<='", "'=='", "'!='", "'&'", "'^'", "'|'", "'&&'", "'||'", 
-		"'.'", "'['", "']'", null, null, null, null, null, null, null, "'bool'", 
-		"'int'", "'string'", "'void'", "'null'", "'true'", "'false'", "'if'", 
-		"'else'", "'for'", "'while'", "'break'", "'continue'", "'return'", "'new'", 
-		"'class'", "'this'"
-	};
-	private static final String[] _SYMBOLIC_NAMES = {
-		null, null, null, null, null, null, null, null, null, null, null, null, 
-		null, null, null, null, null, null, null, null, null, null, null, null, 
-		null, null, null, null, null, null, null, null, null, "LogicConstant", 
-		"PosIntegerConstant", "StringConstant", "LineComment", "BlockComment", 
-		"WhiteSpace", "NewLine", "Bool", "Int", "String", "Void", "Null", "True", 
-		"False", "If", "Else", "For", "While", "Break", "Continue", "Return", 
-		"New", "Class", "This", "Identifier"
-	};
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "';'", "'('", "')'", "','", "'='", "'{'", "'}'", "'++'", "'--'", 
+			"'+'", "'-'", "'!'", "'~'", "'*'", "'/'", "'%'", "'<<'", "'>>'", "'>'", 
+			"'>='", "'<'", "'<='", "'=='", "'!='", "'&'", "'^'", "'|'", "'&&'", "'||'", 
+			"'.'", "'['", "']'", null, null, null, null, null, null, null, "'bool'", 
+			"'int'", "'string'", "'void'", "'null'", "'true'", "'false'", "'if'", 
+			"'else'", "'for'", "'while'", "'break'", "'continue'", "'return'", "'new'", 
+			"'class'", "'this'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, null, null, null, null, null, null, null, null, null, null, null, 
+			null, null, null, null, null, null, null, null, null, null, null, null, 
+			null, null, null, null, null, null, null, null, null, "LogicConstant", 
+			"PosIntegerConstant", "StringConstant", "LineComment", "BlockComment", 
+			"WhiteSpace", "NewLine", "Bool", "Int", "String", "Void", "Null", "True", 
+			"False", "If", "Else", "For", "While", "Break", "Continue", "Return", 
+			"New", "Class", "This", "Identifier"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
 	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
 
 	/**
